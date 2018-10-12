fdce23969bff0a936168f22ff3584daf767fa51e
Karan Kotwal <kotwalkaran64@gmail.com>
2018-10-11 21:14:07 -0400

    Example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        [4, 2, 3, 4, 5, 6, 7, 8, 9],
+        [2, 2, 3, 4, 5, 6, 7, 8, 9],
+        [3, 2, 3, 4, 5, 6, 7, 8, 9],
+        [2, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 2, 3, 4, 5, 6, 7, 8, 9],
+        [2, 2, 3, 4, 5, 6, 7, 8, 9],
+        [3, 2, 3, 4, 5, 6, 7, 8, 9],
+        [4, 2, 3, 4, 5, 6, 7, 8, 9]
+      ]
+    };
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {data.map((aSudokuRow, index) => (
+          <tr key={`sudokuRow-${index}`}>
+            {aSudokuRow.map((aSudokuCell, cellIndex) => (
+              <td key={`sudokuCell-${cellIndex}`}>{aSudokuCell}</td>
+            ))}
+          </tr>
+        ))}
       </div>
     );
   }
